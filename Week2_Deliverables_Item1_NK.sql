e22c0cc6d9d5e5bc8ada330056a1a1909ce945db
ShortLeggedFlamingo <149968074+ShortLeggedFlamingo@users.noreply.github.com>
2026-08-30 00:12:52 -0500

    Add files via upload

--- Week2_Deliverables_Item1_NK.sql
+++ Week2_Deliverables_Item1_NK.sql
@@ -1,3 +1,6 @@
+USE Restaurants;
+GO
+
 /* 
     Creating the table for the dishes served at the restaurant.
     I'm leaving this fairly open ended incase we need to add more
@@ -6,11 +9,17 @@
 
 CREATE TABLE Dishes(
 
-    DishNum int IDENTITY (1,1) PRIMARY KEY,
-    DishName nvarchar(50) NOT NULL,
-    Notes nvarchar(100)
+    DishID INT IDENTITY (1,1) NOT NULL,
+    DishName NVARCHAR(50) NOT NULL,
+    Price DECIMAL(10,2) NOT NULL,
+    Notes NVARCHAR(100),
+    DateAdded DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
 
-)
+    -- constraints to enforce data integrity
+    CONSTRAINT PK_Dishes PRIMARY KEY (DishID)
+
+);
+GO
 
 /* 
     Creating the table for ingredients used in the recipes.
@@ -20,12 +29,17 @@ CREATE TABLE Dishes(
 
 CREATE TABLE Ingredients(
 
-    IngredientNum int IDENTITY (1,1) PRIMARY KEY,
-    IngredientName nvarchar(30) NOT NULL,
-    IngredientCost money NOT NULL,
-    QuantityStocked int NOT NULL
+    IngredientID INT IDENTITY (101,1),
+    IngredientName NVARCHAR(30) NOT NULL,
+    IngredientCost DECIMAL(10,2) NOT NULL,
+    QuantityStocked INT NOT NULL,
+    DateAdded DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
 
-)
+    -- constraints to enforce data integrity
+    CONSTRAINT PK_Ingredients PRIMARY KEY (IngredientID)
+
+);
+GO
 
 /* 
     Creating the table for the recipes used at the restaurant
@@ -33,13 +47,21 @@ CREATE TABLE Ingredients(
 
 CREATE TABLE Recipes(
 
-    RecipeNum int IDENTITY(1,1) PRIMARY KEY,
-    DishNum int references Dishes(DishNum) NOT NULL,
-    IngredientNum int references Ingredients(IngredientNum) NOT NULL,
-    QuantityUsed int NOT NULL,
-    DateAdded DATETIME DEFAULT GETDATE()
+    RecipeID INT IDENTITY(1,1),
+    DishID INT REFERENCES Dishes(DishID) NOT NULL,
+    IngredientID INT REFERENCES Ingredients(IngredientID) NOT NULL,
+    QuantityUsed INT NOT NULL,
+    DateAdded DATETIME2 NOT NULL DEFAULT SYSDATETIME()
 
-)
+    -- constraints to enforce data integrity
+    CONSTRAINT PK_Recipes PRIMARY KEY (RecipeID)
+    CONSTRAINT FK_Recipes_DishID
+        FOREIGN KEY (DishID) REFERENCES Dishes(DishID)
+    CONSTRAINT CK_Recipes_QuantityUsed_Positive
+        CHECK (QuantityUsed > 0)
+
+);
+GO
 
 /* 
     Creating the table for menus.
@@ -50,22 +72,17 @@ CREATE TABLE Recipes(
 
 CREATE TABLE Menu(
 
-    MenuNum INT IDENTITY (1,1) PRIMARY KEY,
-    InUse nvarchar(3) NOT NULL,
-    DateAdded DATETIME DEFAULT GETDATE()
+    MenuID INT IDENTITY (10,1),
+    RecipeID INT REFERENCES Recipes(RecipeID) NOT NULL,
+    InUse NVARCHAR(3) NOT NULL,
+    DateAdded DATETIME2 NOT NULL DEFAULT SYSDATETIME()
 
-)
+    -- constraints to enforce data integrity
+    CONSTRAINT PK_Menu PRIMARY KEY (MenuID)
+    CONSTRAINT FK_Menu_RecipeID
+        FOREIGN KEY (RecipeID) REFERENCES Recipes(RecipeID)
+    CONSTRAINT CK_Menu_InUse_Status
+        CHECK (LOWER(InUse) IN ('yes','no'))
 
-/* 
-    Creating table linking the recipes to the menus.
-*/
-
-CREATE TABLE MenuRecipes(
-
-    RecipeNum int references Recipes(RecipeNum) NOT NULL,
-    MenuNum int references Menu(MenuNum) NOT NULL,
-    DateAdded DATETIME DEFAULT GETDATE(),
-    Notes nvarchar(100),
-    PRIMARY KEY(RecipeNum, MenuNum)
-
-)
+);
+GO
